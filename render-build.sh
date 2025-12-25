c28e959750532d01f706b80abbeabd0aebbf668b
rak9b <mdrakibulislam7018@gmail.com>
2025-12-25 21:49:12 +0600

    Fix: Final Render Build Script

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+﻿#!/usr/bin/env bash
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -6,4 +6,3 @@ set -o nounset
 npm install
 npm run build
 npx prisma generate
-# npx prisma migrate deploy # Keeping disabled for SQLite/Safe Mode. Enable if using Postgres.
